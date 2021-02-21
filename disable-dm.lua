5ea86dde5f87b8d16c7b0c734744dbcd88d72756
Just'Sky <65964146+Skyez@users.noreply.github.com>
2021-02-21 11:36:48 +0100

    Delete disable-dm.lua

--- disable-dm.lua
+++
@@ -1,6 +0,0 @@
-{{/*Trigger type: Command*/}}
-{{/*Trigger: disable-dm*/}}
-{{/*Role Restrictions: Role allowing the advertisement verification*/}}
-
-✅ You have correctly disabled the reminder of punishment in DM !
-{{dbSet .User.ID "DM" .User.ID}}
