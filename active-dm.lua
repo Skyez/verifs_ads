4de42cf15e1e8d0ac7e1dccb3fd7c44cfb901f67
Just'Sky <65964146+Skyez@users.noreply.github.com>
2021-02-21 11:37:20 +0100

    Delete active-dm.lua

--- active-dm.lua
+++
@@ -1,6 +0,0 @@
-{{/*Trigger type: Command*/}}
-{{/*Trigger: active-dm*/}}
-{{/*Role Restrictions: Role allowing the advertisement verification*/}}
-
-✅ You have correctly activated the reminder of punishment in DM !
-{{dbDel .User.ID "DM"}}
